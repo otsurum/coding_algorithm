5eecde86a9df885195edf4cb883d37256ae3bd20
鶴見駿 <architecturent1005@gmail.com>
2026-03-14 16:54:31 +0900

    feat: ct_1

--- ct/0314.playground/Contents.swift
+++ ct/0314.playground/Contents.swift
@@ -1,3 +1,68 @@
-import Cocoa
+func readInt() -> Int {
+    return Int(readLine()!)!
+}
 
-var greeting = "Hello, playground"
+
+
+enum Command: String {
+    case MOVE
+    case QUERY_EAST
+    case QUERY_NORTH
+}
+
+let N = readInt()
+
+var eastPassedLog: [Int] = [0]
+var northPassedLog: [Int] = [0]
+
+var nowEastPosition = 0
+var nowNorthPosition = 0
+
+func passedCount(point: Int, log: [Int]) -> Int {
+    var copyLog = log
+    var nowPoint = copyLog.removeFirst()
+    
+    var passCount = 0
+    for log in copyLog {
+        if log > nowPoint {
+            if nowPoint < point && log >= point {
+                passCount += 1
+            }
+        } else {
+            if nowPoint > point && log <= point {
+                passCount += 1
+            }
+        }
+        
+        nowPoint = log
+    }
+    
+    return passCount
+}
+
+for _ in 0..<N {
+    let commandString = String(readLine()!)
+    if commandString.hasPrefix(Command.MOVE.rawValue) {
+        let parts = commandString.split(separator: " ")
+        let a = Int(parts[1])!
+        let b = Int(parts[2])!
+        
+        nowEastPosition += a
+        nowNorthPosition += b
+
+        eastPassedLog.append(nowEastPosition)
+        northPassedLog.append(nowNorthPosition)
+    } else if commandString.hasPrefix(Command.QUERY_EAST.rawValue) {
+        let parts = commandString.split(separator: " ")
+        let p = Int(parts[1])!
+        
+        let count = passedCount(point: p, log: eastPassedLog)
+        print(count)
+    } else {
+        let parts = commandString.split(separator: " ")
+        let q = Int(parts[1])!
+        
+        let count = passedCount(point: q, log: northPassedLog)
+        print(count)
+    }
+}
